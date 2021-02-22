8069f49ffcd14c4cc65cdad83e00c495da09dcb1
Adarsh Rajput <adarshrajput@Adarshs-MacBook-Pro.local>
2021-02-22 11:30:58 +0530

    this is adarsh

--- src/main/java/com/example/demo/SpringdemoApplication.java
+++ src/main/java/com/example/demo/SpringdemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(SpringdemoApplication.class, args);
+
 	}
 
 }
